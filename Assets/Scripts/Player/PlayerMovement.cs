bc9087d531ee5bf7912621e87a867b51af5136f0
Law47 <Law47ben@gmail.com>
2026-02-09 14:11:40 -0800

    Initial Unity project commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private PlayerInputActions inputActions;
+
+    private Vector2 moveInput;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        inputActions = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+
+        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += _ => moveInput = Vector2.zero;
+
+        inputActions.Player.Jump.performed += _ => Jump();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (!isGrounded) return;
+
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+}
